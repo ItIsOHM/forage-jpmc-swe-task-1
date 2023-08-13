4d42d946c38377863b3ce2fe4b74c978b2e10846
Rhythm Garg <rhythmgarg05@gmail.com>
2023-08-13 20:57:51 +0530

    Fix Task 1

--- client3.py
+++ client3.py
@@ -57,6 +57,6 @@ if __name__ == "__main__":
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
-            price[stock] = price
+            prices[stock] = price
 
         print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
